e3e28931f44006206311a916777df14e63932672
Omri Hopson <omri.hopson@gmail.com>
2019-10-07 17:39:31 -0500

    implemented front end

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from "react";
+import { async } from "q";
+import axios from 'axios';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  // constructor(props) {
+
+  //   super(props);
+  //   this.state = {value: 'coconut'};
+
+  //   this.handleChange = this.handleChange.bind(this);
+    
+  // }
+
+  // handleChange(event) {
+  //   this.setState({value: event.target.value});
+  // }
+
+  const sayHello = () => {
+    console.log("hello");
+  };
+
+  useEffect( () => {
+    activateCipher();
+  }, []);
+
+  const vigenerURL = "http://localhost:8080/api/cipher/encrypt";
+
+  const Key = 'test';
+  const rawtext = 'she sells sea shells';
+  const encrypt = true;
+
+  const [text, setText] = useState("Text");
+
+  const activateCipher = async () => {
+    const response = await axios.post(
+      vigenerURL,
+      { Key: Key, rawtext: rawtext, encrypt: encrypt},
+      { headers: { 'Content-Type': 'application/json' } }
+    )
+    console.log(response.data.text)
+
+    const responseData = await response;
+    console.log(responseData);
+  }
+
+
+
+  return(
+    <div>
+      <h1 className="">Hello Vigener Cipher</h1>
+      <div className="">
+        <form>
+          <label> Enter Text Here: </label>
+          <input type="text" name="text"/>
+          <select>
+            <option value="true">Encrypt Text</option>
+            <option value="false">Decrypt Text</option>
+          </select>
+          <input type="submit" value="Submit" />
+        </form>
+        <h1 className=""> Your text has been processed and here is your result: {}</h1>
+      </div>
     </div>
   );
 }
