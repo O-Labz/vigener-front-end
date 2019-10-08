c00f52f20671665f56add3f67acea5fb3d322c9a
Omri Hopson <omri.hopson@gmail.com>
2019-10-07 19:17:32 -0500

    implemented states

--- src/App.js
+++ src/App.js
@@ -1,65 +1,75 @@
 import React, {useEffect, useState} from "react";
-import { async } from "q";
 import axios from 'axios';
 
 
 function App() {
 
-  // constructor(props) {
+  // useEffect( () => {
+  //   activateCipher();
+  // }, []);
 
-  //   super(props);
-  //   this.state = {value: 'coconut'};
+  const vigenerURL = 'http://localhost:8080/api/cipher/encrypt';
 
-  //   this.handleChange = this.handleChange.bind(this);
-    
-  // }
+  // const Key = 'test';
+  // const rawtext = 'she sells sea shells';
+  // const encrypt = true;
 
-  // handleChange(event) {
-  //   this.setState({value: event.target.value});
-  // }
 
-  const sayHello = () => {
-    console.log("hello");
-  };
+  const [value, setValue] = useState('');
+  const [rawtext, setRawText] = useState('');
+  const [Key, setKey] = useState('');
+  const [encrypt, setEncrypt] = useState('');
+  const [message, setMessage] = useState('');
 
-  useEffect( () => {
-    activateCipher();
-  }, []);
-
-  const vigenerURL = "http://localhost:8080/api/cipher/encrypt";
-
-  const Key = 'test';
-  const rawtext = 'she sells sea shells';
-  const encrypt = true;
-
-  const [text, setText] = useState("Text");
 
   const activateCipher = async () => {
-    const response = await axios.post(
-      vigenerURL,
-      { Key: Key, rawtext: rawtext, encrypt: encrypt},
-      { headers: { 'Content-Type': 'application/json' } }
-    )
-    console.log(response.data.text)
+    const response = await axios.post(vigenerURL, 
+      { Key: Key, rawtext: rawtext, encrypt: encrypt},{ headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' } })
+    .then(response => {
+      console.log(response)
+    })
+    .catch(error => {
+      console.log(error);
+    });
 
-    const responseData = await response;
+    const responseData = response;
     console.log(responseData);
   }
 
 
+  const getRawText = e => {
+    setRawText(e.target.value);
+  }
+
+  const getKey = e => {
+    setKey(e.target.value);
+  }
+
+  const getEncrypt = e => {
+    setEncrypt(e.target.value);
+  }
+
 
   return(
+
     <div>
       <h1 className="">Hello Vigener Cipher</h1>
       <div className="">
         <form>
           <label> Enter Text Here: </label>
-          <input type="text" name="text"/>
-          <select>
-            <option value="true">Encrypt Text</option>
-            <option value="false">Decrypt Text</option>
-          </select>
-          <input type="submit" value="Submit" />
+          <div>
+            <input type="text" name="text" value={rawtext} onChange = {getRawText}/>
+          </div>
+          <div>
+            <input type="text" name="key" value={getKey}/>
+          </div>
+          <div>
+            <select onChange = {getEncrypt}>
+              <option value="true">Encrypt Text</option>
+              <option value="false">Decrypt Text</option>
+            </select>
+          </div>
+          <div> <input type="submit" value="Submit" /> </div>
         </form>
         <h1 className=""> Your text has been processed and here is your result: {}</h1>
       </div>
